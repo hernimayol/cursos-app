dd1420e33a9731c7612e06bee71f4445e40ccc81
Hernan <jhernanmt@gmail.com>
2025-06-09 11:28:38 -0300

    Primer Commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+
+    Route::apiResource('categories', CategoryController::class)->except(['show']);
+});
